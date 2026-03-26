51897bf2aa91b6f78a01c9a7e9d96304b0c750c2
arksg <yuka.towa.1306@gmail.com>
2026-03-26 20:54:30 +0900

    Feat: テキスト主体のページを Markdown で作成できるようにする

---
+++ lib/markdown.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+
+const contentDirectory = path.join(process.cwd(), "content");
+
+export async function getMarkDownContent(fileName: string): Promise<{ contentHtml: string; [key: string]: any } | null> {
+  const fullPath = path.join(contentDirectory, fileName);
+  if (!fs.existsSync(fullPath)) {
+    return null;
+  }
+  
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // gray-matter を利用してメタデータセクションを解析
+  const matterResult = matter(fileContents);
+
+  // remark を利用して Markdown を HTML に変換
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    contentHtml,
+    ...(matterResult.data as { [key: string]: any }),
+  };
+}
