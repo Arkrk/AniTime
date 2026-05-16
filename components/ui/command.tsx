f137a835b4eca3fdd2473d4db70fd844e5174e1c
arksg <yuka.towa.1306@gmail.com>
2026-05-17 03:27:33 +0900

    Update: モバイルサイズでは Command コンポーネントを画面上部に表示する

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -54,7 +54,7 @@ function CommandDialog({
       </DialogHeader>
       <DialogContent
         className={cn(
-          "top-1/3 translate-y-0 overflow-hidden rounded-4xl! p-0",
+          "top-4 sm:top-1/3 translate-y-0 overflow-hidden rounded-4xl! p-0",
           className
         )}
         showCloseButton={showCloseButton}
