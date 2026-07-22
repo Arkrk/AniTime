065bebc21ae33b6abe609c68aa3763dee9b0b767
arksg <yuka.towa.1306@gmail.com>
2026-07-23 01:50:34 +0900

    Fix: スケルトンスクリーンのアニメーション開始位置を修正

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -5,7 +5,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="skeleton"
       className={cn(
-        "relative overflow-hidden rounded-2xl bg-muted after:absolute after:inset-y-0 after:left-0 after:w-[1000%] after:animate-shimmer after:bg-size-[50%_100%] after:bg-[linear-gradient(90deg,transparent_25%,rgba(0,0,0,0.06)_50%,transparent_75%)] dark:after:bg-[linear-gradient(90deg,transparent_25%,rgba(255,255,255,0.1)_50%,transparent_75%)]",
+        "relative overflow-hidden rounded-2xl bg-muted after:absolute after:inset-y-0 after:left-0 after:w-[400%] after:animate-shimmer after:bg-size-[50%_100%] after:bg-[linear-gradient(90deg,transparent_50%,rgba(0,0,0,0.06)_75%,transparent_100%)] dark:after:bg-[linear-gradient(90deg,transparent_50%,rgba(255,255,255,0.1)_75%,transparent_100%)]",
         className
       )}
       {...props}
